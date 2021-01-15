573a27dd71ceacf69a2daf14c4a89727ad823c7e
josemarmendes <josemarmendes17@gmail.com>
2021-01-15 00:20:31 -0300

    adicionando firebase do google ao projeto para upload de arquivos

---
+++ src/main/java/com/carros/api/upload/UploadController.java
@@ -0,0 +1,25 @@
+package com.carros.api.upload;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carros.domain.upload.FirebaseStorageService;
+
+@RestController
+@RequestMapping("/api/v1/upload")
+public class UploadController {
+	
+	@Autowired
+	private FirebaseStorageService firebaseService;
+	
+	@PostMapping
+	public ResponseEntity upload(@RequestBody UploadInput uploadInput) {
+		String url = firebaseService.upload(uploadInput);
+		
+		return ResponseEntity.ok(new UploadOutput(url));
+	}
+}
---
+++ src/main/java/com/carros/domain/upload/FirebaseStorageService.java
@@ -0,0 +1,68 @@
+package com.carros.domain.upload;
+
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import com.carros.api.upload.UploadInput;
+
+@Service
+public class FirebaseStorageService {
+	
+	 @PostConstruct
+	    private void init() throws IOException {
+
+	        if (FirebaseApp.getApps().isEmpty()) {
+	            InputStream in =
+	                    FirebaseStorageService.class.getResourceAsStream("/serviceAccountKey.json");
+
+	            System.out.println(in);
+
+	            if(in != null) {
+	                FirebaseOptions options = new FirebaseOptions.Builder()
+	                        .setCredentials(GoogleCredentials.fromStream(in))
+	                        .setStorageBucket("carros-afe4f.appspot.com")
+	                        .setDatabaseUrl("https://carros-afe4f.firebaseio.com")
+	                        .build();
+
+	                FirebaseApp.initializeApp(options);
+	            
+	            } else {
+	                System.err.println("Configure o arquivo serviceAccountKey.json do Firebase NOT FOUND!");
+	            
+	            }
+	        }
+	    }
+
+
+	public String upload(UploadInput uploadInput) {
+		Bucket bucket = StorageClient.getInstance().bucket();
+        System.out.println(bucket);
+
+//        Blob blob = bucket.create("nome.txt","Ricardo Ninja Lecheta".getBytes(), "text/html");
+
+        byte[] bytes = Base64.getDecoder().decode(uploadInput.getBase64());
+
+        String fileName = uploadInput.getFileName();
+        Blob blob = bucket.create(fileName, bytes, uploadInput.getMimeType());
+
+        // Assina URL válida por N dias
+        //URL signedUrl = blob.signUrl(1, TimeUnit.DAYS);
+
+        // Deixa URL pública
+        blob.createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+
+        return String.format("https://storage.googleapis.com/%s/%s", bucket.getName(), fileName);
+	}
+
+}

f4cba2d4d541e3cab497a99e50fdb36b4c505086
josemarmendes <josemarmendes17@gmail.com>
2021-01-26 00:06:51 -0300

    adicionando token jwt para autenticação na api

--- src/main/java/com/carros/api/security/SecurityConfig.java
+++ src/main/java/com/carros/api/security/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.carros.api.security;
 
+import com.carros.api.security.jwt.JwtAuthenticationFilter;
+import com.carros.api.security.jwt.JwtAuthorizationFilter;
+import com.carros.api.security.jwt.handler.AccessDeniedHandler;
+import com.carros.api.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,22 +22,35 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	@Qualifier("userDetailsService")
 	private UserDetailsService userDetailsService;
 
+	@Autowired
+	private UnauthorizedHandler unauthorizedHandler;
+
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
+		AuthenticationManager authManager = authenticationManager();
+
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
+				.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+				.anyRequest().authenticated().and().csrf().disable().addFilter(new JwtAuthenticationFilter(authManager))
+				.addFilter(new JwtAuthorizationFilter(authManager, userDetailsService)).exceptionHandling()
+				.accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
+
 		auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
-		
+
 		/*
 		 * auth .inMemoryAuthentication().passwordEncoder(encoder)
 		 * .withUser("user").password(encoder.encode("user")).roles("USER") .and()
